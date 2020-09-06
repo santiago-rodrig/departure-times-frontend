e36896c1fee43e81449bc8194bc58cd2e7752b51
Santiago <santo1996.29@gmail.com>
2020-09-06 19:58:18 -0400

    refactor: lift state to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import RoutesSelect from './RoutesSelect';
+import { useSelector } from 'react-redux';
+import { selectRoutes, populateAsync } from '../app/states/routes/routesSlice';
 
-const Dashboard = ({ routes }) => (
-  <div id="dashboard">
-    <RoutesSelect routes={routes} />
-  </div>
-);
+
+const Dashboard = () => {
+  const [selectedRoute, setSelectedRoute] = useState(null);
+  const dispatch = useDispatch();
+  const routes = useSelector(selectRoutes);
+  let displayedResult = undefined;
+
+  // This effects only is executed once
+  useEffect(() => {
+    dispatch(populateAsync());
+  }, [dispatch]);
+
+  // This handles the change event over the routes select
+  const handleRoutesSelectChange = (e) => {
+    setSelectedRoute(e.target.value);
+  }
+
+  // This part conditionally sets the returned JSX
+  if (selectedRoute === null && routes.status === 'success') {
+    setSelectedRoute(routes.value[0].tag);
+  } else if (selectedRoute !== null) {
+    displayedResult = (
+      <RoutesSelect
+        routes={routes}
+        route={selectedRoute}
+        handleChange={handleRoutesSelectChange}
+      />
+    );
+  } else if (routes.status === 'fetching') {
+    displayedResult = (
+      <p>
+        Fetching data...
+      </p>
+    );
+  } else {
+    displayedResult = (
+      <p>
+        Sorry, the data could not be obtained. :(
+      </p>
+    );
+  }
+
+  return (
+    <div id="dashboard">
+      {displayedResult}
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/components/RoutesSelect.js
+++ src/components/RoutesSelect.js
@@ -1,52 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { populateAsync } from '../app/states/routes/routesSlice';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 
-const RoutesSelect = ({ routes }) => {
-  const dispatch = useDispatch();
-  const [selectedRoute, setSelectedRoute] = useState(null);
-  let displayedResult = undefined;
-
-  // This effects only is executed once
-  useEffect(() => {
-    dispatch(populateAsync());
-  }, [dispatch]);
-
-  // This handles the change events over the select React element
-  const handleRoutesSelectChange = (e) => {
-    setSelectedRoute(e.target.value);
-  }
-
-  // This part conditionally sets the returned JSX
-  if (selectedRoute === null && routes.status === 'success') {
-    displayedResult = (
-      <select value={routes.value[0].tag} onChange={handleRoutesSelectChange}>
-        {routes.value.map(route => <option key={route.tag} value={route.tag}>{route.title}</option>)}
-      </select>
-    );
-
-    setSelectedRoute(routes.value[0].tag);
-  } else if (selectedRoute !== null) {
-    displayedResult = (
-      <select value={selectedRoute} onChange={handleRoutesSelectChange}>
-        {routes.value.map(route => <option key={route.tag} value={route.tag}>{route.title}</option>)}
-      </select>
-    );
-  } else if (routes.status === 'fetching') {
-    displayedResult = (
-      <p>
-        Fetching data...
-      </p>
-    );
-  } else {
-    displayedResult = (
-      <p>
-        Sorry, the data could not be obtained. :(
-      </p> 
-    );
-  }
-
-  return displayedResult;
-};
+const RoutesSelect = ({ routes, route, handleChange }) => (
+  <select value={route.tag} onChange={handleChange}>
+    {routes.map(r => <option key={r.tag} value={r.tag}>r.title</option>)}
+  </select>
+);
 
 export default RoutesSelect;
