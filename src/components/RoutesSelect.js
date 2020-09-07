6a155b0c95c16f998351b66fbb2cd8fab53ead0e
Santiago <santo1996.29@gmail.com>
2020-09-06 21:34:47 -0400

    feature: start fetching route data on routes select change

--- src/components/RoutesSelect.js
+++ src/components/RoutesSelect.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const RoutesSelect = ({ routes, route, handleChange }) => (
-  <select onChange={handleChange} placeholder="Please select a route">
-    {routes.map(r => <option key={r.tag} value={r.tag}>r.title</option>)}
-  </select>
-);
+const RoutesSelect = ({ routes, route, setRoute, setFetchRouteData }) => {
+  let result = undefined;
+
+  const handleChange = (e) => {
+    setRoute(e.target.value);
+    e.target.value = route;
+    setFetchRouteData(true);
+  }
+
+  if (routes.status === 'fetching') {
+    result = <p>Fetching routes...</p>;
+  } else if (routes.status === 'success') {
+    result = (
+      <select onChange={handleChange} value={route || 'placeholder'}>
+        <option value="placeholder">-- Please select a route --</option>
+        {routes.value.map(r => <option key={r.tag} value={r.tag}>{r.title}</option>)}
+      </select>
+    );
+  }
+
+  return result;
+};
 
 export default RoutesSelect;
